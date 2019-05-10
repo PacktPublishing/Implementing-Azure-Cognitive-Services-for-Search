ab75513831365f1e4734fcec64b26853a89ae6e8
Peter Rombouts <prombouts@gmail.com>
2019-05-10 15:42:15 +0200

    Added Start project for Section 4

---
+++ Section 4/Create an application to interpret your local image files - Start/ConsoleAppVisualSearch/Program.cs
@@ -0,0 +1,40 @@
+﻿using Microsoft.Azure.CognitiveServices.Search.VisualSearch;
+using Microsoft.Azure.CognitiveServices.Search.VisualSearch.Models;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Net;
+
+namespace ConsoleAppVisualSearch
+{
+    class Program
+    {
+        private static readonly string ApiKey = "<enter your S9 key here>";
+        private static VisualSearchRequest GetSearchRequest(string url = "", string token = "")
+        {
+            if (!string.IsNullOrEmpty(url))
+            {
+                return new VisualSearchRequest
+                {
+                    ImageInfo = new ImageInfo
+                    {
+                        Url = url
+                    }
+                };
+            }
+
+            return new VisualSearchRequest
+            {
+                ImageInfo = new ImageInfo
+                {
+                    ImageInsightsToken = token
+                }
+            };
+        }
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World");
+        }
+    }
+}
